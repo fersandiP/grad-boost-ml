f82be4d712bc09456b3d0f35a4f8276461e57531
damartanjung <damartanjung@gmail.com>
2018-04-20 10:44:27 +0700

    Add implementation of gradient boosting

---
+++ gradientBoosting.py
@@ -0,0 +1,57 @@
+
+
+columnNames = ['HouseVal','MedInc','HouseAge','AveRooms','AveBedrms','Population','AveOccup','Latitude','Longitud']
+
+df = pd.read_csv('cadata.txt',skiprows=27, sep='\s+',names=columnNames)
+
+
+xi = x # initialization of input
+yi = y # initialization of target
+# x,y --> use where no need to change original y
+ei = 0 # initialization of error
+n = len(yi)  # number of rows
+predf = 0 # initial prediction 0
+
+for i in range(30): # loop will make 30 trees (n_estimators). 
+    tree = DecisionTree(xi,yi) # DecisionTree scratch code can be found in shared github/kaggle link. 
+                               # It just create a single decision tree with provided min. sample leaf
+    tree.find_better_split(0)  # For selected input variable, this splits (<n and >n) data so that std. deviation of 
+                               # target variable in both splits is minimum as compared to all other splits
+    
+    r = np.where(xi == tree.split)[0][0]   #  finds index where this best split occurs
+    
+    left_idx = np.where(xi <= tree.split)[0] # index lhs of split
+    right_idx = np.where(xi > tree.split)[0] # index rhs of split
+    
+    predi = np.zeros(n)
+    np.put(predi, left_idx, np.repeat(np.mean(yi[left_idx]), r))  # replace left side mean y
+    np.put(predi, right_idx, np.repeat(np.mean(yi[right_idx]), n-r))  # right side mean y
+    
+    predi = predi[:,None]  # make long vector (nx1) in compatible with y
+    predf = predf + predi  # final prediction will be previous prediction value + new prediction of residual
+    
+    ei = y - predf  # needed originl y here as residual always from original y    
+    yi = ei # update yi as residual to reloop
+    
+    
+    # plotting after prediction
+    xa = np.array(x.x) # column name of x is x 
+    order = np.argsort(xa)
+    xs = np.array(xa)[order]
+    ys = np.array(predf)[order]
+    
+    #epreds = np.array(epred[:,None])[order]
+
+    f, (ax1, ax2) = plt.subplots(1, 2, sharey=True, figsize = (13,2.5))
+
+    ax1.plot(x,y, 'o')
+    ax1.plot(xs, ys, 'r')
+    ax1.set_title(f'Prediction (Iteration {i+1})')
+    ax1.set_xlabel('x')
+    ax1.set_ylabel('y / y_pred')
+
+    ax2.plot(x, ei, 'go')
+    ax2.set_title(f'Residuals vs. x (Iteration {i+1})')
+    ax2.set_xlabel('x')
+    ax2.set_ylabel('Residuals')
+
